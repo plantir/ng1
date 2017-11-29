4d1006b961cbb6819348fac12064a84e1fa7a4ef
armin <arminkheirkhahan@gmail.com>
2017-11-29 11:23:31 +0330

    change factory from function to class

--- generator/factory/name.factory.ts
+++ generator/factory/name.factory.ts
@@ -4,7 +4,10 @@ export interface I%nameUpper% extends ng.resource.IResource<I%nameUpper%> {
     Id: number;
 }
 export interface I%nameUpper%Resource extends ng.resource.IResourceClass<I%nameUpper%> {}
-export function %nameUpper%_factory($resource: ng.resource.IResourceService, myConstant: IMyConstant): I%nameUpper%Resource {
-    'ngInject';
-    return <I%nameUpper%Resource>$resource(myConstant.ApiUrl + '%name%', {}, {});
+export class %nameUpper%Service {
+    static selector = '$%name%';
+    constructor($resource: ng.resource.IResourceService, myConstant: IMyConstant): I%nameUpper%Resource { 
+        'ngInject';
+        return <I%nameUpper%Resource>$resource(myConstant.ApiUrl + '/%name%', {}, {});
+    }
 }
