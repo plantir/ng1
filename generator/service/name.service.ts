1658b5d8e76ef1348ed324423f1412efae21af05
armin <arminkheirkhahan@gmail.com>
2018-08-05 22:54:44 +0430

    change service

--- generator/service/name.service.ts
+++ generator/service/name.service.ts
@@ -1,8 +1,5 @@
-import { IPromise } from 'angular';
 import { IMyConstant } from '../../core/core.constant';
-
-
-
+import { IHttpPromise } from 'angular';
 export interface I%nameUpper% {
     name?: string;
     email?: string;
@@ -10,19 +7,20 @@ export interface I%nameUpper% {
 }
 
 export interface I%nameUpper%Service {
-    get(id:number): IHttpPromise<I%nameUpper%>;
+    get(id: number): IHttpPromise<I%nameUpper%>;
     save(data: I%nameUpper%): IHttpPromise<I%nameUpper%>;
 }
+
 export class %nameUpper%Service {
-    static selector = '$%name%';
+    static selector = '$%selectorName%';
     constructor(private $http: ng.IHttpService, private $q: ng.IQService, private myConstant: IMyConstant) { 
         'ngInject';
     }
 
-    get(id:number) {
+    get(id: number) {
         return  this.$http.get(`${this.myConstant.ApiUrl}/%name%/${id}`);
     }
-    save(data:I%nameUpper% ) {
+    save(data: I%nameUpper% ) {
         return this.$http.post(`${this.myConstant.ApiUrl}/%name%`, data);
     }
 }
