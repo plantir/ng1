474179154b9f681992a0806669203e508066a0e0
armin <arminkheirkhahan@gmail.com>
2018-08-05 22:59:12 +0430

    change service

--- generator/service/name.service.ts
+++ generator/service/name.service.ts
@@ -2,24 +2,24 @@ import { IMyConstant } from '../../core/core.constant';
 import { IHttpPromise } from 'angular';
 
 export interface I%nameUpper% {
-    id: number;
+  id: number;
 }
 
 export interface I%nameUpper%Service {
-    get(id: number): IHttpPromise<I%nameUpper%>;
-    save(data: I%nameUpper%): IHttpPromise<I%nameUpper%>;
+  get(id: number): IHttpPromise<I%nameUpper%>;
+  save(data: I%nameUpper%): IHttpPromise<I%nameUpper%>;
 }
 
 export class %nameUpper%Service {
-    static selector = '$%selectorName%';
-    constructor(private $http: ng.IHttpService, private myConstant: IMyConstant) {
-        'ngInject';
-    }
+  static selector = '$%selectorName%';
+  constructor(private $http: ng.IHttpService, private myConstant: IMyConstant) {
+    'ngInject';
+  }
 
-    get(id: number) {
-        return  this.$http.get(`${this.myConstant.ApiUrl}/%name%/${id}`);
-    }
-    save(data: I%nameUpper% ) {
-        return this.$http.post(`${this.myConstant.ApiUrl}/%name%`, data);
-    }
+  get(id: number) {
+    return this.$http.get(`${this.myConstant.ApiUrl}/%name%/${id}`);
+  }
+  save(data: I%nameUpper%) {
+    return this.$http.post(`${this.myConstant.ApiUrl}/%name%`, data);
+  }
 }
