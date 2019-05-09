980147c3923ba2040c34f711c5a6ce7fdf265dab
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-05-09 12:32:45 +0430

    change module generator

--- generator/module/name.module.ts
+++ generator/module/name.module.ts
@@ -1,6 +1,6 @@
 import * as angular from 'angular';
-import { routing } from './%name%.routes';
+import { Routing } from './%name%.routes';
 
 export const moduleName = angular
   .module('application.%name%', ['ui.router'])
-  .config(routing).name;
+  .config(Routing).name;
